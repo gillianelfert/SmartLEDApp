3b6098c27b766949abfd624173a4766829ca6e8e
Gillian Elfert <gillian.elfert@haw-hamburg.de>
2023-06-26 02:06:24 +0200

    ProfileTest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.arch.core:core-testing:2.1.0")
+    androidTestImplementation("androidx.test:core:1.4.0")
 
     implementation("com.hivemq:hivemq-mqtt-client:1.3.0")
 
